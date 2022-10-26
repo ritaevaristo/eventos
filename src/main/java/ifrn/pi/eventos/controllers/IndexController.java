7240a4c5b41e91b61b2d1bda6de7a882b5deb998
Maria Rita da Silva Evaristo <m.evaristo@academico.ifrn.edu.br>
2022-10-26 16:10:42 -0300

    Criando página de detalhes do evento

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("chamou o método index");
-		return "home";
+		return "redirect:/eventos";
 
 }
 }
